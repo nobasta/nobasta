6c154b65cca8bb5748ab55f068064c69feb02a2a
ladaewoo <ladaewoo@hotmail.com>
2014-04-20 11:02:13 -0500

    las

--- js/js.js
+++ js/js.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 			password: "pass1990",
 			auth: "basic"
 		});
-		console.log($(".post").val());
+		console.log($(".post"));
 	}
 	
 	$("#savePost").click(function(){
