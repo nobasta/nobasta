6bc3876ecd7663026131fbf4f794068fc0d7a22b
ladaewoo <ladaewoo@hotmail.com>
2014-04-20 21:46:55 -0500

    las

--- _plugins/base64.rb
+++ _plugins/base64.rb
@@ -22,7 +22,7 @@ Liquid::Template.register_tag('youtube', Jekyll::YouTubeTag)
 module Jekyll
   module Decode
     def decode(msg)
-      decoded = Base64.decode64(msg)
+		decoded = Base64.decode64(msg)
 	  decoded = decoded.encode!( 'UTF-8', invalid: :replace, undef: :replace, replace: ' ' )
 	 end
   end
