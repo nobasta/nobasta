e6110d0ce9373fa033bfe11d2bd077743f3814b9
ladaewoo <ladaewoo@hotmail.com>
2014-06-06 18:34:38 -0500

    las

--- js/js.js
+++ js/js.js
@@ -24,8 +24,8 @@ var curr_date, curr_month, date, curr_year, urlPost, repo, postUrl, fecha, githu
 	
 	
 	function formulario(form){
-	console.log('hola');
-	console.log(form);/*
+	
+	alert(form);/*
 	contentP = $("#contentP").val();	
 	titleP = $("#titleP").val();
 	contenido = 
