a3b09f676343fb93d682435be5af01c76a6f237f
ladaewoo <ladaewoo@hotmail.com>
2014-06-07 09:07:25 -0500

    las

--- js/js.js
+++ js/js.js
@@ -5,9 +5,7 @@ var curr_date, curr_month, date, curr_year, urlPost, repo, postUrl, fecha, githu
                     selector:'textarea',
                     plugins: "image, link, media, print",
                     toolbar: "styleselect | undo redo | media | image | removeformat | bold italic underline |  aligncenter alignjustify  | bullist numlist outdent indent | link | print | fontselect fontsizeselect",
-                    menubar: false,
-                    statusbar: true,
-                    resize: true
+                 
        
 		});
 		$('.modal-content').draggable();
