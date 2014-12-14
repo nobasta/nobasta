930a97aed68c0c20d6cff6afbb67dac158711497
ladaewoo <ladaewoo@hotmail.com>
2014-12-13 19:40:12 -0600

    las

--- js/js.js
+++ js/js.js
@@ -67,9 +67,10 @@ var curr_date, curr_month, date, curr_year, urlPost, repo, postUrl, fecha, githu
 if(!d.getElementById(id)){js=d.createElement(s);js.id=id;
 js.src="https://platform.twitter.com/widgets.js";
 fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-49346522-2', 'enmexicoserinocentenobasta.tk');
+ga('create', 'UA-49346522-2', 'nobasta.github.io');
 ga('send', 'pageview');
